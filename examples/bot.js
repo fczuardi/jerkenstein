99893021d784eafe2acf32515f14fe7ccb4bede7
Fabricio C Zuardi <fabricio@fabricio.org>
2010-09-12 12:13:56 -0300

    plug now accepts arrays of plugs as arguments, plus new example plugin and bot example now loads everything under the plugins dir

--- examples/bot.js
+++ examples/bot.js
@@ -1,20 +1,26 @@
+//config
 var options = { 
        server: 'irc.freenode.net'
       ,nick: 'bullwinkle'
       ,channels: ['#bullwinkle-dev', "#external-ning-2.0"]
     }
 
+//libraries
 var  jerkenstein = require('../lib/jerkenstein')
+    ,fs = require('fs')
+    ,path = require('path');
 
-jerkenstein.plug(
-   require('../examples/plugins/echo').plugin
-  ,require('../examples/plugins/calc').plugin
-  ,require('../examples/plugins/double').plugin
-  ,require('../examples/plugins/colorize').plugin
-  ,require('../examples/plugins/rainbow').plugin
-  );
+//global vars
+var plugins_dir = path.join(__dirname, 'plugins');
 
-var blame = require('../examples/plugins/blame').plugin
-
-jerkenstein.plug(blame);
-jerkenstein(function(){}).connect(options)
+//init
+fs.readdir(plugins_dir, function(err, files){
+  if (err) throw err;
+  //load all plugins
+  files.forEach(function(filename){
+    filename = path.join(plugins_dir, filename)
+    jerkenstein.plug(require(filename.slice(0,-3)).plugin)
+  });
+  //connect
+  jerkenstein(function(){}).connect(options)
+});
---
+++ examples/plugins/upper.js
@@ -0,0 +1,10 @@
+this.plugin = {
+   'name': 'upper'
+  ,'pattern': /^\?upper (.+)$/ 
+  ,'description': 'Uppercase a message.'
+  ,'example': '?upper consequences will never be the same.'
+  ,'action': function(message) {
+    message.say(message.match_data[1].toUpperCase())
+  }
+  ,'url': 'http://gist.github.com/576171'
+}
