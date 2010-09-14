21cc78767f791bbc9a5250a02e6b61427eaacd04
Fabricio C Zuardi <fabricio@fabricio.org>
2010-09-14 14:00:41 -0300

    engine and version evironment variables plus an engine plugin

---
+++ examples/plugins/engine.js
@@ -0,0 +1,10 @@
+this.plugin = {
+   'name': 'engine'
+  ,'pattern': /^\?engine\s*$/ 
+  ,'description': 'Information about the engine of the current bot.'
+  ,'example': '?engine'
+  ,'action': function(message, env) {
+    message.say('Engine: '+env.engine+'\nVersion: '+env.version);
+  }
+  ,'url': ''
+}
